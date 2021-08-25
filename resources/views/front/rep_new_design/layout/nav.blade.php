1e397f6656c285c81ab8e0caee6fb06f10971737
naimatuly162 <sonchoyita18@gmail.com>
2021-08-25 20:17:51 +0600

    nav, body

--- resources/views/front/rep_new_design/layout/nav.blade.php
+++ resources/views/front/rep_new_design/layout/nav.blade.php
@@ -1,115 +1,85 @@
-   <header>
-       <!-- Navbar -->
-       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-           <!-- Container wrapper -->
-           <div class="container">
-               <!-- Toggle button -->
-               <button
-                   class="navbar-toggler"
-                   type="button"
-                   data-mdb-toggle="collapse"
-                   data-mdb-target="#navbarSupportedContent"
-                   aria-controls="navbarSupportedContent"
-                   aria-expanded="false"
-                   aria-label="Toggle navigation"
-               >
-                   <i class="fas fa-bars"></i>
-               </button>
-               <!-- Collapsible wrapper -->
-               <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                   <!-- Navbar brand -->
-                   <a class="navbar-brand btn-m mt-2 mt-lg-0" href="#">CELT<span> Abroad</span>
-                       <img
-                           src="#"
-                           height="15"
-                           alt=""
-                           loading="lazy"
-                       />
-                   </a>
-                   <!-- Left links -->
-                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                       <li class="nav-item">
-                           <a class="nav-link" href="#">Institutes</a>
-                       </li>
-                       <li class="nav-item">
-                           <a class="nav-link" href="#">Programs</a>
-                       </li>
-                       <li class="nav-item">
-                           <a class="nav-link" href="#">Applicatins</a>
-                       </li>
-                       <li class="nav-item">
-                           <a class="nav-link" href="#">Students</a>
-                       </li>
-                   </ul>
-                   <!-- Left links -->
-               </div>
-               <!-- Collapsible wrapper -->
+<header>
+    <!-- Navbar -->
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="navbar nav-m navbar-expand-lg navbar-light bg-light">
+                    <!-- Toggle button -->
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-mdb-toggle="collapse"
+                        data-mdb-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    <!-- Collapsible wrapper -->
+                    <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+                        <!-- Navbar brand -->
+                        <a class="navbar-brand btn-m mt-2 mt-lg-0" href="#">CELT<span> Abroad</span>
+                        </a>
+                        <!-- Left links -->
+                        <ul class="navbar-nav nav-middle me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><span>Institutes</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Programs</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Applications</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Students</a>
+                            </li>
+                        </ul>
+                        <!-- Left links -->
 
-               <!-- Right elements -->
-               <div class="d-flex align-items-center">
-                   <!-- Icon -->
-                   <!-- Notifications -->
-                   <a
-                       class="text-reset me-3 dropdown-toggle hidden-arrow"
-                       href="#"
-                       id="navbarDropdownMenuLink"
-                       role="button"
-                       data-mdb-toggle="dropdown"
-                       aria-expanded="false"
-                   >
-                       <i class="fas fa-bell"></i>
-                       <span class="badge rounded-pill badge-notification bg-danger">1</span>
-                   </a>
-                   <ul
-                       class="dropdown-menu dropdown-menu-end"
-                       aria-labelledby="navbarDropdownMenuLink"
-                   >
-                       <li>
-                           <a class="dropdown-item" href="#">Some news</a>
-                       </li>
-                       <li>
-                           <a class="dropdown-item" href="#">Another news</a>
-                       </li>
-                       <li>
-                           <a class="dropdown-item" href="#">Something else here</a>
-                       </li>
-                   </ul>
-                   <!-- Avatar -->
-                   <a
-                       class="dropdown-toggle d-flex align-items-center hidden-arrow"
-                       href="#"
-                       id="navbarDropdownMenuLink"
-                       role="button"
-                       data-mdb-toggle="dropdown"
-                       aria-expanded="false"
-                   >
-                       <img
-                           src="https://mdbootstrap.com/img/new/avatars/2.jpg"
-                           class="rounded-circle"
-                           height="25"
-                           alt=""
-                           loading="lazy"
-                       />
-                   </a>
-                   <ul
-                       class="dropdown-menu dropdown-menu-end"
-                       aria-labelledby="navbarDropdownMenuLink"
-                   >
-                       <li>
-                           <a class="dropdown-item" href="#">My profile</a>
-                       </li>
-                       <li>
-                           <a class="dropdown-item" href="#">Settings</a>
-                       </li>
-                       <li>
-                           <a class="dropdown-item" href="#">Logout</a>
-                       </li>
-                   </ul>
-               </div>
-               <!-- Right elements -->
-           </div>
-           <!-- Container wrapper -->
-       </nav>
+                        <!-- Right elements -->
+                        <div class="d-flex right-nav align-items-center">
+                            <!-- Icon -->
+                            <!-- Notifications -->
+                            <a
+                                class="text-reset me-3 dropdown-toggle hidden-arrow"
+                                href="#"
+                                id="navbarDropdownMenuLink"
+                                role="button"
+                                data-mdb-toggle="dropdown"
+                                aria-expanded="false"
+                                style=" border-right: 1px solid rgba(30, 57, 143, 0.2);
+                                margin-right: 33px;"
+                            >
+                                <i class="fa fa-bell" style="margin-right: 33px;"></i>
+{{--                                <span class="badge rounded-pill badge-notification bg-danger">1</span>--}}
+                            </a>
+                            <!-- Avatar -->
+                            <a
+                                class="dropdown-toggle d-flex align-items-center hidden-arrow"
+                                href="#"
+                                id="navbarDropdownMenuLink"
+                                role="button"
+                                data-mdb-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                <img
+                                    src="https://mdbootstrap.com/img/new/avatars/2.jpg"
+                                    class="rounded-circle"
+                                    height="25"
+                                    alt=""
+                                    loading="lazy"
+                                />
+                            </a>
+                        </div>
+                        <!-- Right elements -->
+                    </div>
+                    <!-- Collapsible wrapper -->
+                </nav>
+            </div>
+        </div>
+    </div>
 
     <!-- Navbar -->
 
@@ -122,36 +92,36 @@
             linear-gradient(89.95deg, rgba(238, 113, 0, 0.5) 0.04%, rgba(30, 57, 143, 0.5) 101%),
             url('{{asset('images/bg-banner.svg')}}');
             ">
-        <div class="mask" >
+        <div class="mask">
             <div class="d-flex justify-content-center align-items-center h-100">
                 <div class="text-white">
                     <h1 class="mb-3">Institutes</h1>
-                    <a class="btn btn-outline-light btn-lg " href="#!" role="button"
-                    >Add Institute</a
+                    <a class="btn btn-outline-light btn-lg " href="#" role="button"
+                    ><i class="fas fa-plus"></i>Add Institute</a
                     >
                 </div>
             </div>
         </div>
     </div>
     <!-- Background image -->
-       <div class="navbar navbar-light bg-light">
-           <div class="container">
-               <a class="navbar-brand">
-                   <button type="button" class="btn btn-warning">Recent</button>
-                   <button type="button" class="btn btn-light">Old</button>
-               </a>
-               <form class="d-flex input-group w-auto">
-                   <input
-                       type="search"
-                       class="form-control rounded"
-                       placeholder="Search"
-                       aria-label="Search"
-                       aria-describedby="search-addon"
-                   />
-                   <span class="input-group-text border-0" id="search-addon">
+    <div class="navbar nav-bottom navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand">
+                <button type="button" class="btn btn-theme-orange" ><span>Recent</span></button>
+                <button type="button" class="btn btn-default">Old</button>
+            </a>
+            <form class="d-flex input-group w-auto">
+                <input
+                    type="search"
+                    class="form-control rounded"
+                    placeholder="Search"
+                    aria-label="Search"
+                    aria-describedby="search-addon"
+                />
+                <span class="input-group-text border-0" id="search-addon">
         <i class="fas fa-search"></i>
       </span>
-               </form>
-           </div>
-       </div>
+            </form>
+        </div>
+    </div>
 </header>
--- resources/views/front/rep_new_design/pages/institute.blade.php
+++ resources/views/front/rep_new_design/pages/institute.blade.php
@@ -4,7 +4,26 @@
 <div class="container-fluid">
     <div class="container">
         <div class="row">
-            <div class="col-sm">
+            <div class="col-md-4 col-sm-6">
+                <div class="card" style="width: 18rem;">
+                    <img  src="{{asset('images/image_13.png')}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Harvard University</h5>
+                        <h6 class="card-text">
+                            Cambridge,MA,USA
+                        </h6>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><i class="fa fa-user"></i>Total Student (52)</li>
+                        <li class="list-group-item"><i class="fa fa-file"></i>Total Dicipline (08)</li>
+                    </ul>
+                    <div class="card-body">
+                        <button href="#" class="btn btn-default"><i class="fa fa-edit"></i>Edit</button>
+                        <button href="#" class="btn btn-default"><i class="fa fa-copy"></i>Duplicate</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
                 <div class="card" style="width: 18rem;">
                     <img src="..." class="card-img-top" alt="...">
                     <div class="card-body">
@@ -22,25 +41,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">An item</li>
-                        <li class="list-group-item">A second item</li>
-                        <li class="list-group-item">A third item</li>
-                    </ul>
-                    <div class="card-body">
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm">
+            <div class="col-md-4 col-sm-6">
                 <div class="card" style="width: 18rem;">
                     <img src="..." class="card-img-top" alt="...">
                     <div class="card-body">
@@ -59,6 +60,23 @@
                 </div>
             </div>
         </div>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </div>
 
